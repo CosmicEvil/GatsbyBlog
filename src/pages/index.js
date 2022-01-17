1ace9461267f890b1a23ac495028bb5a7b898f00
“Wouter <woutermassie@gmail.com>
2022-01-17 15:51:35 +0100

    mainpage wat bijgewerkt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import './main-index.scss'
 const MainPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
@@ -12,9 +12,163 @@ const MainPage = ({ data, location }) => {
     <Seo title="All posts" />
     <h1>Welcome to {siteTitle}</h1>
     
-    <p>This is the new way of learning karate</p>
-    
-  </Layout>
+    <h3>This is the new way of learning karate</h3>
+    <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    <div className="mainBlock">
+
+    <div className="block wide">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/White_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>White belt</h4>
+      <p>First level of the DojoDAO - <br /> Unlock the first course and an NFT!</p>
+    </div>
+    </div> 
+
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/Yellow_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Yellow belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/orange_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Orange belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+    <div className="block wide">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/green_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Green belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/blue_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Blue belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+   
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/red_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+        <div className="right">
+
+      <h4>Red belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>    
+    </div> 
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/purple_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Purple belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+    <div className="block">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/brown_belt.png"
+               width={150}
+               height={150}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Purple belt</h4>
+      <p>You can earn new belts here by doing various tasks, and by unlocking a new belt, you will also unlock new courses, content and even some goodies!</p>
+    </div>
+    </div> 
+    <div className="block wide">
+    <StaticImage
+               className="belt"
+               layout="fixed"
+               formats={["auto", "webp", "avif"]}
+               src="../images/black_belt_super.png"
+               width={260}
+               height={200}
+               quality={95}
+               alt="Logo"
+    />
+    <div className="right">
+      <h4>Black belt</h4>
+      <p>You <b>FUCKING MADE IT!!</b><br />The endgame!</p>
+    </div>
+    </div>
+    </div>
+    <div className="mainBlock footer">
+      <h2>Ready To start Learning???</h2>
+      <button className="main-button">Yes send me a white belt!</button>
+    </div>
+    </Layout>
      
 
   )
